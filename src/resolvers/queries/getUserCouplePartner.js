64493392d83e9d71da68aaff0bb3df351a51699e
Jana Rajakumar <jana.rajakumar@utoronto.ca>
2021-06-14 00:28:25 -0400

    Refactor getCouplePartner

--- src/resolvers/queries/getUserCouplePartner.js
+++ src/resolvers/queries/getUserCouplePartner.js
@@ -3,16 +3,14 @@ import { PrismaSelect } from '@paljs/plugins'
 export default async (parent, args, context, info) => {
   const { select } = new PrismaSelect(info).value
 
-  let userCouple = await context.prisma.couple.findUnique({
-    where: { id: parent.id }
-  }).userTwo({ select: { ...select, id: true } })
+  // return user
+  const userCouple = await context.prisma.couple.findUnique({
+    where: { id: parent.id },
+    include: {
+      userOne: { select: { ...select, id: true } },
+      userTwo: { select: { ...select, id: true } }
+    }
+  })
 
-  if (!userCouple || userCouple.id === parent.partnerID) {
-    // try with userTOne
-    userCouple = await context.prisma.couple.findUnique({
-      where: { id: parent.id }
-    }).userOne({ select: { ...select, id: true } })
-  }
-
-  return userCouple
+  return parent.partnerID === userCouple.userTwo.id ? userCouple.userOne : userCouple.userTwo
 }
