d1ca3f375ce2b8f0b80f967debba935b9351be8e
Jana Rajakumar <jana.rajakumar@utoronto.ca>
2021-06-19 12:01:21 -0400

    Auto generate username and password for the moment

--- src/resolvers/mutations/addUser.js
+++ src/resolvers/mutations/addUser.js
@@ -7,16 +7,17 @@ import { PrismaSelect } from '@paljs/plugins'
 export default async (parent, args, context, info) => {
   isAdmin(context)
 
-  const { password, ...rest } = args.input
+  const username = args.input.fullName.trim().replace(' ', '_')
 
-  const hashedPassword = await hash(password, 10)
+  const password = await hash('123', 10)
 
   const { select } = new PrismaSelect(info).value
 
   const user = await context.prisma.user.create({
     data: {
-      password: hashedPassword,
-      ...rest
+      username,
+      password,
+      ...args.input
     },
     select: { ...select, id: true }
   })
