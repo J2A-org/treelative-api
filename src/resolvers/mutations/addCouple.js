5cd273649faa01af3b8fa55485c6fce165b8ed15
Jana Rajakumar <jana.rajakumar@utoronto.ca>
2021-06-26 19:02:47 -0400

    During couple creation; amerge any existing children between the couple

--- src/resolvers/mutations/addCouple.js
+++ src/resolvers/mutations/addCouple.js
@@ -35,5 +35,52 @@ export default async (parent, args, context, info) => {
     }
   })
 
+  // if either couple has children - connect any missing ones
+  const userOneChildIDs = (await context.prisma.user.findUnique({
+    where: { id: userOneID },
+    select: { id: true }
+  }).children()).map(({ id }) => id)
+
+  const userTwoChildIDs = (await context.prisma.user.findUnique({
+    where: { id: userTwoID },
+    select: { id: true }
+  }).children()).map(({ id }) => id)
+
+  const userOneChildrenNotInUserTwo = []
+  for (const userID of userOneChildIDs) {
+    if (!userTwoChildIDs.includes(userID)) {
+      userOneChildrenNotInUserTwo.push(userID)
+    }
+  }
+  if (userOneChildrenNotInUserTwo.length > 0) {
+    await context.prisma.user.update({
+      where: { id: userTwoID },
+      data: {
+        children: {
+          connect: userOneChildrenNotInUserTwo.map(id => ({ id }))
+        }
+      },
+      select: { id: true }
+    })
+  }
+
+  const userTwoChildrenNotInUserOne = []
+  for (const userID of userTwoChildIDs) {
+    if (!userOneChildIDs.includes(userID)) {
+      userTwoChildrenNotInUserOne.push(userID)
+    }
+  }
+  if (userTwoChildrenNotInUserOne.length > 0) {
+    await context.prisma.user.update({
+      where: { id: userOneID },
+      data: {
+        children: {
+          connect: userTwoChildrenNotInUserOne.map(id => ({ id }))
+        }
+      },
+      select: { id: true }
+    })
+  }
+
   return couple
 }
--- src/resolvers/queries/getUserFamily.js
+++ src/resolvers/queries/getUserFamily.js
@@ -54,7 +54,6 @@ export default async (parent, args, context, info) => {
                 }
               }
             ]
-
           }
         }
       ]
