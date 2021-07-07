6c1cd3c6072319a6aa4246ccb8c2f5252edec6ce
Jana Rajakumar <jana.rajakumar@utoronto.ca>
2021-07-07 13:33:39 -0400

    Remove crown image for admins

--- src/resolvers/queries/getNetworkData.js
+++ src/resolvers/queries/getNetworkData.js
@@ -11,12 +11,7 @@ export default async (parent, args, context, info) => {
     id: user.id,
     label: user.shortName,
     group: user.role === 'ADMIN' ? 'admin' : 'individual',
-    image:
-      user.id === '187e3fbf-347e-4e0d-9a82-b0b2cf2bc57f'
-        ? 'https://res.cloudinary.com/arun99-dev/image/upload/v1624889786/adminNode_h3od6t.png'
-        : user.id === '4105def9-79cb-4e4b-b653-e5f713c80682'
-          ? 'https://res.cloudinary.com/arun99-dev/image/upload/v1624891187/admin2Node_lfsl37.png'
-          : `https://${process.env.MINIO_ENDPOINT}/avatar/${user.id}.jpg`,
+    image: `https://${process.env.MINIO_ENDPOINT}/avatar/${user.id}.jpg`,
     brokenImage: `https://ui-avatars.com/api/?name=${user.fullName}&background=random&rounded=true&font-size=0.5&bold=true`
   }))
 
