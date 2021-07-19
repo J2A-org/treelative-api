8513ede4774da3ecc88e4b44931727d60497e4bf
Jana Rajakumar <jana.rajakumar@utoronto.ca>
2021-07-19 19:10:48 -0400

    Revert last commit; TODO: add user preference isPublic profile

--- src/resolvers/queries/getUser.js
+++ src/resolvers/queries/getUser.js
@@ -3,19 +3,17 @@ import { ApolloError } from 'apollo-server'
 import { PrismaSelect } from '@paljs/plugins'
 
 export default async (parent, args, context, info) => {
+  // only authenticated users can view a user
+  if (!context.user) {
+    throw new ApolloError('You must be authenticated to perform this action', 'UNAUTHENTICATED')
+  }
+
   const { select } = new PrismaSelect(info).value
 
   const user = await context.prisma.user.findUnique({
     where: args.filter,
     select: { ...select, id: true }
   })
-
-  if (user.role !== 'ADMIN') {
-    // only authenticated users can view a regular user
-    if (!context.user) {
-      throw new ApolloError('You must be authenticated to perform this action', 'UNAUTHENTICATED')
-    }
-  }
 
   return user
 }
