a71bd0080385929608aafb4cd778d7a6cb7a0ebf
Arun <friday99@outlook.it>
2021-07-21 15:59:22 +0200

    fixed avatar missing for some users & added brokenAvatar

--- src/resolvers/queries/getAgeData.js
+++ src/resolvers/queries/getAgeData.js
@@ -9,9 +9,20 @@ export default async (parent, args, context, info) => {
     const birthYear = user.dateOfBirth ? new Date(user.dateOfBirth).toLocaleString('default', { year: 'numeric' }) : null
     if (!birthYear) continue
     if (result[birthYear]) {
-      result[birthYear] = [...result[birthYear], user]
+      result[birthYear] = [
+        ...result[birthYear],
+        {
+          ...user,
+          avatar: `https://${process.env.MINIO_ENDPOINT}/avatar/${user.id}.jpg`,
+          brokenAvatar: `https://ui-avatars.com/api/?name=${user.fullName}&background=random&rounded=true&font-size=0.5&bold=true`
+        }
+      ]
     } else {
-      result[birthYear] = [{ ...user, avatar: `https://${process.env.MINIO_ENDPOINT}/avatar/${user.id}.jpg` }]
+      result[birthYear] = [{
+        ...user,
+        avatar: `https://${process.env.MINIO_ENDPOINT}/avatar/${user.id}.jpg`,
+        brokenAvatar: `https://ui-avatars.com/api/?name=${user.fullName}&background=random&rounded=true&font-size=0.5&bold=true`
+      }]
     }
   }
 
