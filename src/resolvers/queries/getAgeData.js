e2a1ac9291672490651c6bbfbe588ccd2f58c3a2
Arun <friday99@outlook.it>
2021-07-21 15:20:37 +0200

    removed avatar from getAgeData (myBad)

--- src/resolvers/queries/getAgeData.js
+++ src/resolvers/queries/getAgeData.js
@@ -1,6 +1,6 @@
 export default async (parent, args, context, info) => {
   const users = await context.prisma.user.findMany({
-    select: { id: true, shortName: true, fullName: true, avatar: true, dateOfBirth: true }
+    select: { id: true, shortName: true, fullName: true, dateOfBirth: true }
   })
 
   const result = {}
