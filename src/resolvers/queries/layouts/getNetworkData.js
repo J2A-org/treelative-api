23cf35d22b3303084b852b76ce8c86248b5397a6
Jana Rajakumar <jana.rajakumar@utoronto.ca>
2021-07-30 23:56:42 -0400

    Complete getNetworkData query

--- src/resolvers/queries/layouts/getNetworkData.js
+++ src/resolvers/queries/layouts/getNetworkData.js
@@ -1,5 +1,5 @@
 export default async (parent, args, context, info) => {
-  const users = await context.models.User.find({}).lean()
+  const users = await context.models.User.find({}).populate('partner').lean()
 
   const nodeUsers = users.map(user => ({
     id: user._id,
@@ -9,25 +9,41 @@ export default async (parent, args, context, info) => {
     brokenImage: `https://ui-avatars.com/api/?name=${user.fullName}&background=random&rounded=true&font-size=0.5&bold=true`
   }))
 
-  // const couples = []
-  // const nodeCouples = couples.map(couple => ({
-  //   id: couple.id,
-  //   group: 'couple'
-  // }))
+  const couplesMap = {}
+  for (const user of users) {
+    if (user.partner) {
+      const coupleID = [user._id, user.partner._id].sort().join('-')
+      if (!couplesMap[coupleID]) {
+        couplesMap[coupleID] = {
+          id: coupleID,
+          group: 'couple',
+          coupleOne: user,
+          coupleTwo: user.partner,
+          children: [...user.children, ...user.partner.children]
+        }
+      }
+    }
+  }
 
-  // const nodeEdges = couples.map(couple => (
-  //   [
-  //     { from: couple.userOneID, to: couple.id, color: '#F10037' },
-  //     { from: couple.userTwoID, to: couple.id, color: '#F10037' },
-  //     ...couple.userOne.children.map(child => (
-  //       { from: couple.id, to: child.id, color: '#07E901' }
-  //     ))
-  //   ]
-  // )).flat()
+  const couples = Object.values(couplesMap)
+  const nodeCouples = couples.map(couple => ({
+    id: couple.id,
+    group: 'couple'
+  }))
+
+  const nodeEdges = couples.map(couple => (
+    [
+      { from: couple.coupleOne._id, to: couple.id, color: '#F10037' },
+      { from: couple.coupleTwo._id, to: couple.id, color: '#F10037' },
+      ...couple.children.map(child => (
+        { from: couple.id, to: child._id.toString(), color: '#07E901' }
+      ))
+    ]
+  )).flat()
 
   // create a dataset with nodes & edges
-  const nodes = [...nodeUsers]
-  const edges = []
+  const nodes = [...nodeUsers, ...nodeCouples]
+  const edges = [...nodeEdges]
 
   return { nodes, edges }
 }
