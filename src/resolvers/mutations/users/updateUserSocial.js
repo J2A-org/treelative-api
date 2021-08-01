21f37d86ba4cf6bc734655ddb2bca2a974fb308a
Jana Rajakumar <jana.rajakumar@utoronto.ca>
2021-08-01 15:51:23 -0400

    Fix user social update bug

--- src/resolvers/mutations/users/updateUserSocial.js
+++ src/resolvers/mutations/users/updateUserSocial.js
@@ -7,9 +7,14 @@ export default async (parent, args, context, info) => {
     throw new ApolloError('You are not authorized to perform this action', 'UNAUTHORIZED')
   }
 
+  const updateObject = {}
+  for (const key in args.input) {
+    updateObject[`social.${key}`] = args.input[key]
+  }
+
   const user = await context.models.User.findOneAndUpdate(
     { _id: args.userID },
-    { social: args.input },
+    { $set: updateObject },
     { new: true }
   )
 
