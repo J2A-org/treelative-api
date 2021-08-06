72ff521b53947227f0c7685f650acb74b3bae987
Jana Rajakumar <jana.rajakumar@utoronto.ca>
2021-08-06 14:04:36 -0400

    Spread people apart

--- src/resolvers/queries/layouts/getMapData.js
+++ src/resolvers/queries/layouts/getMapData.js
@@ -1,14 +1,28 @@
+const adjustLocation = (count, { lat, lng }) => {
+  return {
+    lat: lat + 0.00007 * count,
+    lng: lng + 0.00007 * count
+  }
+}
+
 export default async (parent, args, context, info) => {
   const users = await context.models.User.find({ currentLocation: { $ne: null } }).lean()
 
-  const usersMap = users.map(user => ({
-    id: user._id,
-    shortName: user.shortName,
-    fullName: user.fullName,
-    image: `https://${process.env.MINIO_ENDPOINT}/avatar/${user._id}.jpg`,
-    brokenImage: `https://ui-avatars.com/api/?name=${user.fullName}&background=random&rounded=true&font-size=0.5&bold=true`,
-    position: user.currentLocation.parsed.geometry.location
-  }))
+  const seenLocations = {}
+
+  const usersMap = users.map(user => {
+    const locationKey = Object.values(user.currentLocation.parsed.geometry.location).join('-')
+    const result = {
+      id: user._id,
+      shortName: user.shortName,
+      fullName: user.fullName,
+      image: `https://${process.env.MINIO_ENDPOINT}/avatar/${user._id}.jpg`,
+      brokenImage: `https://ui-avatars.com/api/?name=${user.fullName}&background=random&rounded=true&font-size=0.5&bold=true`,
+      position: seenLocations[locationKey] ? adjustLocation(seenLocations[locationKey], user.currentLocation.parsed.geometry.location) : user.currentLocation.parsed.geometry.location
+    }
+    seenLocations[locationKey] = (seenLocations[locationKey] || 0) + 1
+    return result
+  })
 
   return usersMap
 }
