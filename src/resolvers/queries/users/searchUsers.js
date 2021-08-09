edc656f510d092f4a0c572a3f91c6807d08e554e
Jana Rajakumar <jana.rajakumar@utoronto.ca>
2021-08-09 13:57:56 -0400

    Enhance search query to perform multiple tokens

--- src/resolvers/queries/users/searchUsers.js
+++ src/resolvers/queries/users/searchUsers.js
@@ -2,7 +2,9 @@ import fuzzySearch from '../../../utils/fuzzySearch'
 
 export default async (parent, args, context, info) => {
   const users = await context.models.User.find({
-    $or: fuzzySearch(args.query)
+    $and: [
+      ...args.query.split(' ').map(fuzzySearch).map(q => ({ $or: q }))
+    ]
   }).limit(5).lean()
 
   return users
